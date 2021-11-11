7ad06fc59f423996e5617784c9c4c0aac569cd1b
Alek <aleksebastian@gmail.com>
2021-11-10 20:52:06 -0600

    Created function to recreate SQL db

--- database/postgres/init.js
+++ database/postgres/init.js
@@ -8,19 +8,34 @@ const config = {
   port: process.env.DBPORT,
 };
 
-pgtools.createdb(config, process.env.DBNAME).then((err, res) => {
-  if (err) {
-    console.error(err);
+const createDb = async () => {
+  try {
+    const createDbRes = await pgtools.createdb(config, process.env.DBNAME);
+    console.log(createDbRes);
+    return;
+  } catch (e) {
+    console.error(e);
     process.exit(-1);
   }
-  console.log(res);
-});
+};
 
-// Comment function above and uncomment function below to drop database when running createSqlDb
-// pgtools.dropdb(config, process.env.DBNAME).then((err, res) => {
-//   if (err) {
-//     console.error(err);
-//     process.exit(-1);
-//   }
-//   console.log(res);
-// });
+const dropDb = async () => {
+  try {
+    const dropDbRes = await pgtools.dropdb(config, process.env.DBNAME);
+    console.log(dropDbRes);
+    return;
+  } catch (e) {
+    console.error(e);
+    process.exit(-1);
+  }
+};
+
+const reCreateDb = async () => {
+  const drop = await dropDb();
+  const create = await createDb();
+  return;
+};
+
+// createDb();
+// Drop previously created db and create a new one by commenting the function above and uncommenting the function below before running createSqlDb. Use with caution.
+reCreateDb();
